b00a9cb34ebe984dbf624378ac18644a73dec0a7
ZZMISKING <57690855+ZZMISKING@users.noreply.github.com>
2023-04-13 17:03:45 +0800

    更新 diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -9,6 +9,10 @@
 # File name: diy-part2.sh
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 
+# 修改默认主机名
+sed -i '/uci commit system/i\uci set system.@system[0].hostname='H66K'' package/lean/default-settings/files/zzz-default-settings
+ 
+
 # 修改 argon 为默认主题,可根据你喜欢的修改成其他的（不选择那些会自动改变为默认主题的主题才有效果）
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 
